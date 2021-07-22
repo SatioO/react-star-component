fc2e9b1ce01ab613b7303ef73b68f3184a381f85
SatioO <vaibhav.satam29991@gmail.com>
2021-07-22 20:01:07 +0530

    2 files modified

--- src/Rating/index.js
+++ src/Rating/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Star from './Star';
 
 export default function Rating(props) {
+  function onRate(ev) {
+    console.log({ ev: ev.target.parentNode });
+  }
+
   return (
-    <div>
-      <Star />
+    <div className="rating">
+      {[...Array(props.numberOfStars).keys()].map(n => (
+        <span className="star" key={n + 1} data-value={n + 1} onClick={onRate}>
+          <Star />
+        </span>
+      ))}
     </div>
   );
 }
