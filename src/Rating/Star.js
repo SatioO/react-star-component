7722fa482d90872c293eb274fc5026e975cfde42
SatioO <vaibhav.satam29991@gmail.com>
2021-07-22 23:18:53 +0530

    2 files modified, 1 added

--- src/Rating/Star.js
+++ src/Rating/Star.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 export default function Star(props) {
   return (
-    <svg height="25" width="23" className="star">
-      <polygon
-        points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
-        fill="red"
-      />
+    <svg height="25" width="23" fill="currentColor">
+      <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" />
     </svg>
   );
 }
--- src/Rating/index.js
+++ src/Rating/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import Star from './Star';
+import './star.css';
 
 export default function Rating(props) {
+  const [currentRating, setCurrentRating] = React.useState(props.defaultRating);
+  const star = React.useRef(null);
+
+  React.useEffect(() => {
+    setRating();
+  }, []);
+
+  function setRating() {
+    const stars = star.current.getElementsByClassName('star');
+    Array.from(stars).forEach(star => {
+      star.style.color = currentRating >= star.dataset.value ? 'red' : 'gray';
+    });
+  }
+
+  function onMouseHover(ev) {
+    
+    const stars = ev.target.parentElement.getElementsByClassName('star');
+    const hoverValue = ev.target.dataset.value;
+    Array.from(stars).forEach(star => {
+      star.style.color = hoverValue >= star.dataset.value ? 'red' : 'gray';
+    });
+  }
+
   function onRate(ev) {
-    console.log({ ev: ev.target.parentNode });
+    const rating = parseInt(ev.currentTarget.dataset.value);
+
+    setCurrentRating(rating);
   }
 
   return (
-    <div className="rating">
+    <div className="rating" ref={star}>
       {[...Array(props.numberOfStars).keys()].map(n => (
-        <span className="star" key={n + 1} data-value={n + 1} onClick={onRate}>
+        <span
+          className="star"
+          key={n + 1}
+          data-value={n + 1}
+          onClick={onRate}
+          onMouseOver={onMouseHover}
+        >
           <Star />
         </span>
       ))}
